2d1d7f805c3461a1e9133e9e013acadb4fe61780
Jaytee <Padilla.Jaytee@gmail.com>
2020-05-15 09:29:50 -0700

    Progress commit. Attempted ring buffer code. Can't figure it out

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,30 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if there are no nodes in the doubly linked list
+        if not self.storage.head:
+            self.storage.add_to_head(item)
+        else:
+            self.storage.add_to_tail(item)
+        # elif len(self.storage) > self.capacity:
+        #     self.storage.add_to_tail(item)
+        # else:
+        #     self.storage.remove_from_tail()
+        #     self.storage.add_to_head(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        # if the storage is empty, return empty contents
+        if self.storage.head.value is None:
+            return list_buffer_contents
+        else:
+            start = self.storage.head
+            while start is not None:
+                list_buffer_contents.append(start.value)
+                start = start.next
 
         return list_buffer_contents
 
@@ -30,3 +47,13 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+rb = RingBuffer(5)
+rb.append(1)
+rb.append(9)
+rb.append(7)
+print(rb.storage.length)
+print(rb.storage.head.next.value)
+print(rb.get())
+print(rb.capacity)
+
